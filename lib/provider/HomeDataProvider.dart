58236821bb5a66e3afc4338ded913581463a5b06
Santosh Pokharel <santoshp@walawala.io>
2021-07-07 21:12:09 +0545

    changes in Home menu dialog

---
+++ lib/provider/HomeDataProvider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeDataProvider extends ChangeNotifier {
+  List<String> menuList = [
+    "About Us",
+    "Contact Us",
+    "Blog",
+    "Affiliate Program",
+    "Terms of Use",
+    "Shipping & Return Policy",
+    "FAQs",
+    "Manual"
+  ];
+  
+}
--- lib/ui/screens/dashboard/home/components/HomeMenuDialog.dart
+++ lib/ui/screens/dashboard/home/components/HomeMenuDialog.dart
@@ -1,57 +1,71 @@
+import 'package:export_nepal/provider/HomeDataProvider.dart';
 import 'package:export_nepal/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeMenuDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.white,
-          margin: EdgeInsets.only(left: 100),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop(true);
-                },
-                child: Container(
-                    color: Colors.grey.shade400,
-                    padding: EdgeInsets.only(
-                        top: 20, bottom: 20, left: 20, right: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return ChangeNotifierProvider(
+      create: (context) => HomeDataProvider(),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            body: SafeArea(
+              child: Consumer<HomeDataProvider>(
+                builder: (context, provider, child) {
+                  return Container(
+                    color: Colors.white,
+                    margin: EdgeInsets.only(left: 100),
+                    child: Column(
                       children: [
-                        Text(
-                          "Menu",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pop(true);
+                          },
+                          child: Container(
+                              color: Colors.grey.shade400,
+                              padding: EdgeInsets.only(
+                                  top: 20, bottom: 20, left: 20, right: 10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "Menu",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  Icon(
+                                    Icons.close,
+                                    size: 30,
+                                    color: Colors.black,
+                                  ),
+                                ],
+                              )),
+                        ),
+                        Expanded(
+                          child: ListView.separated(
+                            itemCount: provider.menuList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return HomeDialogItems(title: provider.menuList[index]);
+                            },
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    new Divider(color: kColorBlack, height: 2),
                           ),
                         ),
-                        Icon(
-                          Icons.close,
-                          size: 30,
-                          color: Colors.black,
-                        ),
                       ],
-                    )),
+                    ),
+                  ); //padding: EdgeInsets.all(15),
+                },
               ),
-              Expanded(
-                child: ListView.separated(
-                  itemCount: 5,
-                  itemBuilder: (BuildContext context, int index) {
-                    return HomeDialogItems(title: "name");
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      new Divider(color: kColorBlack, height: 2),
-                ),
-              ),
-            ],
-          ),
-        ),
-        //padding: EdgeInsets.all(15),
+            ),
+          );
+        },
       ),
     );
   }
