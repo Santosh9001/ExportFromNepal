2c7a81029679180cc0413dfeb27698572b17d8a8
Santosh Pokharel <santoshp@walawala.io>
2021-07-20 14:53:29 +0545

    changes in search

--- lib/provider/SearchProvider.dart
+++ lib/provider/SearchProvider.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SearchProvider extends ChangeNotifier{
-  
+class SearchProvider extends ChangeNotifier {
+  List<String> filters = ['All', 'Exporter', 'Wholesaler', 'Service'];
+
+  int _radioGroupValue = 0;
+  int get getRadioGroupValue => _radioGroupValue;
+
+  void setRadioGroupValue(value) {
+    _radioGroupValue = value;
+    notifyListeners();
+  }
+
+  String selectedValue = 'All';
+
+  setValue(value) {
+    selectedValue = value;
+    print(value);
+    notifyListeners();
+  }
 }
--- lib/ui/screens/search/SearchUI.dart
+++ lib/ui/screens/search/SearchUI.dart
@@ -1,4 +1,5 @@
 import 'package:export_nepal/provider/SearchProvider.dart';
+import 'package:export_nepal/ui/screens/dashboard/home/components/ProductCardSmall.dart';
 import 'package:export_nepal/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -46,14 +47,15 @@ class _SearchUIState extends State<SearchUI> {
                             Expanded(
                               child: Padding(
                                 padding: EdgeInsets.fromLTRB(0, 0, 0, 0.0),
-                                child: TextField(                                
+                                child: TextField(
                                   style: kTextStyleRegularPrimary,
                                   textInputAction: TextInputAction.none,
                                   decoration: InputDecoration(
                                     contentPadding: EdgeInsets.all(10.0),
                                     hintText: "Search Here",
                                     suffixIcon: Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
                                       mainAxisSize: MainAxisSize.min,
                                       children: [
                                         InkWell(
@@ -64,18 +66,51 @@ class _SearchUIState extends State<SearchUI> {
                                             height: 25,
                                           ),
                                         ),
-                                        SizedBox(width: 10,),
-                                        InkWell(
-                                          onTap: () {
-                                            
-                                          },
-                                          child: SvgPicture.asset(
-                                            "assets/images/iconly_light_outline_filter.svg",
-                                            allowDrawingOutsideViewBox: true,
-                                            height: 25,
-                                          ),
+                                        SizedBox(
+                                          width: 10,
                                         ),
-                                        SizedBox(width: 15,),
+                                        PopupMenuButton(
+                                            icon: SvgPicture.asset(
+                                              'assets/images/iconly_light_outline_filter.svg',
+                                              height: 25,
+                                            ),
+                                            itemBuilder: (context) => [
+                                                  PopupMenuItem(
+                                                    child: Text(
+                                                      "All",
+                                                      style:
+                                                          kTextStyleMediumPrimary,
+                                                    ),
+                                                    value: "all",
+                                                  ),
+                                                  PopupMenuItem(
+                                                    child: Text(
+                                                      "Exporter",
+                                                      style:
+                                                          kTextStyleMediumPrimary,
+                                                    ),
+                                                    value: "exporter",
+                                                  ),
+                                                  PopupMenuItem(
+                                                    child: Text(
+                                                      "Wholesaler",
+                                                      style:
+                                                          kTextStyleMediumPrimary,
+                                                    ),
+                                                    value: "wholesaler",
+                                                  ),
+                                                  PopupMenuItem(
+                                                    child: Text(
+                                                      "Service",
+                                                      style:
+                                                          kTextStyleMediumPrimary,
+                                                    ),
+                                                    value: "service",
+                                                  ),
+                                                ]),
+                                        SizedBox(
+                                          width: 15,
+                                        ),
                                       ],
                                     ),
                                   ),
@@ -84,15 +119,39 @@ class _SearchUIState extends State<SearchUI> {
                             ),
                           ],
                         ),
-                        SizedBox(height: 15,),
+                        SizedBox(
+                          height: 15,
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text("Your search results for",style: kTextStyleSmallPrimary,),
-                            SizedBox(width: 2,),
-                            Text('"Handmade Dresses"',style: kTextStyleBlueSmall,)
-                            
+                            Text(
+                              "Your search results for",
+                              style: kTextStyleSmallPrimary,
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              '"Handmade Dresses"',
+                              style: kTextStyleBlueSmall,
+                            )
                           ],
+                        ),
+                        SizedBox(height: 15),
+                        Expanded(
+                          child: GridView.builder(
+                            shrinkWrap: true,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 0.6,
+                            ),
+                            itemCount: 30,
+                            itemBuilder: (BuildContext context, int index) {
+                              return ProductCardSmall();
+                            },
+                          ),
                         ),
                       ],
                     ),
